ca07117a6c5ff5f94ccd8477f68ef63292b88909
meenachinmay <chinmayanand896@gmail.com>
2019-11-15 16:53:29 +0530

    express server installed and tested, routes added and tested, mongodb is installed and tested

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./route/api/users');
+const profile = require('./route/api/profile');
+const posts = require('./route/api/posts');
 
 const app = express();
 
+const db = require('./config/keys').mongoURI;
+
+mongoose
+    .connect(db)
+    .then(() => console.log('mongodb connected'))
+    .catch(err => console.log(err));
+
 app.get('/', (req, res) => res.send('Hello!'));
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
